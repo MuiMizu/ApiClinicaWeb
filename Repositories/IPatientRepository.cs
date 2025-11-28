52c13968188d4de7f90d0e5c55b25b0d224d7015
Fabian <hermanoslorenzop2@gmail.com>
2025-11-28 13:24:34 -0400

    Algunos repositorios

---
+++ Repositories/IPatientRepository.cs
@@ -0,0 +1,11 @@
+﻿using APIClinica.Models;
+
+namespace APIClinica.Repositories
+{
+    public interface IPatientRepository : IRepository<Patient>
+    {
+        Task<IEnumerable<Patient>> GetPatientsWithInsuranceAsync();
+        Task<Patient?> GetPatientWithInsuranceByIdAsync(int id);
+        Task<IEnumerable<Patient>> SearchPatientsAsync(string searchTerm);
+    }
+}
---
+++ Repositories/PatientRepository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using APIClinica.Data;
+using APIClinica.Models;
+
+namespace APIClinica.Repositories
+{
+    public class PatientRepository : Repository<Patient>, IPatientRepository
+    {
+        public PatientRepository(ClinicaDbContext context) : base(context)
+        {
+        }
+
+        public async Task<IEnumerable<Patient>> GetPatientsWithInsuranceAsync()
+        {
+            return await _dbSet
+                .Include(p => p.Insurance)
+                .ToListAsync();
+        }
+
+        public async Task<Patient?> GetPatientWithInsuranceByIdAsync(int id)
+        {
+            return await _dbSet
+                .Include(p => p.Insurance)
+                .FirstOrDefaultAsync(p => p.Id == id);
+        }
+
+        public async Task<IEnumerable<Patient>> SearchPatientsAsync(string searchTerm)
+        {
+            return await _dbSet
+                .Include(p => p.Insurance)
+                .Where(p => p.FirstName.Contains(searchTerm) ||
+                           p.LastName.Contains(searchTerm) ||
+                           p.Document.Contains(searchTerm))
+                .ToListAsync();
+        }
+    }
+}
